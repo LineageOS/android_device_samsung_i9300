44378b730efd28a958ac35e3d294e1c04472ff79
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-07-18 16:30:54 +0200

    build tinymix, specify ril properties
    Change-Id: I43e0472e50c32811269df591a740ac6c23f17169

--- galaxys3_base.mk
+++ galaxys3_base.mk
@@ -30,98 +30,99 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Init files
 PRODUCT_COPY_FILES := \
-	device/samsung/i9300/init.bt.rc:root/init.bt.rc \
-	device/samsung/i9300/init.smdk4x12.rc:root/init.smdk4x12.rc \
-	device/samsung/i9300/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
-	device/samsung/i9300/lpm.rc:root/lpm.rc \
-	device/samsung/i9300/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
-	device/samsung/i9300/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
+    device/samsung/i9300/init.bt.rc:root/init.bt.rc \
+    device/samsung/i9300/init.smdk4x12.rc:root/init.smdk4x12.rc \
+    device/samsung/i9300/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
+    device/samsung/i9300/lpm.rc:root/lpm.rc \
+    device/samsung/i9300/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
+    device/samsung/i9300/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
 
 # Camera FW
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/80cfw:system/etc/init.d/80cfw
+    device/samsung/i9300/80cfw:system/etc/init.d/80cfw
 
 # Audio
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/configs/tiny_hw.xml:system/etc/sound/GT-I9300
+    device/samsung/i9300/configs/tiny_hw.xml:system/etc/sound/GT-I9300
 
 # Vold and Storage
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/configs/vold.fstab:system/etc/vold.fstab
+    device/samsung/i9300/configs/vold.fstab:system/etc/vold.fstab
 
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
-	system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
 
 # Wifi
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    device/samsung/i9300/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15
 
 # Gps
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/configs/gps.conf:system/etc/gps.conf \
-	device/samsung/i9300/configs/gps.xml:system/etc/gps.xml
+    device/samsung/i9300/configs/gps.conf:system/etc/gps.conf \
+    device/samsung/i9300/configs/gps.xml:system/etc/gps.xml
 
 # Packages
 PRODUCT_PACKAGES := \
-	audio.primary.exynos4 \
-	audio.a2dp.default \
-	camera.exynos4 \
+    audio.primary.exynos4 \
+    audio.a2dp.default \
+    camera.exynos4 \
     Camera \
     macloader \
     com.android.future.usb.accessory \
     GalaxyS3Settings \
     SamsungServiceMode \
+    tinymix \
     Torch
 
 # HAL
 PRODUCT_PACKAGES += \
-	nfc.exynos4 \
-	lights.exynos4
+    nfc.exynos4 \
+    lights.exynos4
 
-#	libgralloc_ump \
-#	libhwconverter \
-#	libfimg \
-#	hwcomposer.exynos4
-#	libhwjpeg \
-#	libhdmi \
-#	libfimc \
-#	libcec \
-#	libddc \
-#	libedid \
-#	libhdmiclient \
-#	libTVOut
+#   libgralloc_ump \
+#   libhwconverter \
+#   libfimg \
+#   hwcomposer.exynos4
+#   libhwjpeg \
+#   libhdmi \
+#   libfimc \
+#   libcec \
+#   libddc \
+#   libedid \
+#   libhdmiclient \
+#   libTVOut
 #   libtinyalsa \
 
 # NFC
 PRODUCT_PACKAGES += \
-	libnfc \
-	libnfc_jni \
-	Nfc \
-	Tag
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
 
 PRODUCT_COPY_FILES += \
-	packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
-	device/sample/nxp/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
+    device/sample/nxp/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
-	NFCEE_ACCESS_PATH := device/samsung/i9300/nfcee_access.xml
+    NFCEE_ACCESS_PATH := device/samsung/i9300/nfcee_access.xml
 else
-	NFCEE_ACCESS_PATH := device/samsung/i9300/nfcee_access_debug.xml
+    NFCEE_ACCESS_PATH := device/samsung/i9300/nfcee_access_debug.xml
 endif
 
 PRODUCT_COPY_FILES += \
-	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
-	com.android.nfc_extras
+    com.android.nfc_extras
 
 # MFC API
 PRODUCT_PACKAGES += \
@@ -141,70 +142,70 @@ PRODUCT_PACKAGES += \
 #   libOMX.SEC.VP8.Decoder
 
 PRODUCT_COPY_FILES += \
-	device/samsung/i9300/configs/media_profiles.xml:system/etc/media_profiles.xml
+    device/samsung/i9300/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.telephony.ril_class=Smdk4210RIL \
-	mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
+    ro.telephony.ril_class=Smdk4210RIL \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	static_busybox \
-	make_ext4fs \
-	setup_fs
+    static_busybox \
+    make_ext4fs \
+    setup_fs
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-	Galaxy4 \
-	HoloSpiralWallpaper \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	MagicSmokeWallpapers \
-	NoiseField \
-	PhaseBeam \
-	VisualizationWallpapers \
-	librs_jni
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    librs_jni
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-	frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-	frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-	frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-	frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/base/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Feature live wallpaper
 PRODUCT_COPY_FILES += \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.opengles.version=131072 \
-	hwui.render_dirty_regions=false
+    ro.opengles.version=131072 \
+    hwui.render_dirty_regions=false
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
+    persist.sys.usb.config=mtp
 
 $(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
 
