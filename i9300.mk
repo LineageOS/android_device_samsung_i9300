c775350911364b52ff76d3e41f78afbc8632ba74
codeworkx <codeworkx@cyanogenmod.org>
2013-03-26 09:35:18 +0000

    i9300: add SamsungServiceMode
    Change-Id: I774c3f90501f20fc7c1f4eaa61d80c3be7028df5

--- i9300.mk
+++ i9300.mk
@@ -46,9 +46,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     GalaxyS3Settings \
     libsecril-client \
-    libsecril-client-sap
-
-#    SamsungServiceMode
+    libsecril-client-sap \
+    SamsungServiceMode
 
 # NFC
 PRODUCT_PACKAGES += \
