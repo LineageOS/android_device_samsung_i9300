95a7a565d86354420693602674a21b3427cd15a8
codeworkx <codeworkx@cyanogenmod.org>
2013-08-04 17:03:01 +0200

    i9300: recovery: use unified fstab
    Change-Id: I7a988021082955175fefd2fd62ab72f2385f688d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,10 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/i9300/rootdir/fstab.smdk4x12
+RECOVERY_FSTAB_VERSION := 2
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := m0,i9300,GT-I9300
 
