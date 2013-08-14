d80ea95cf578f1cc75a10f37cb62308685a11a44
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-08-14 20:02:51 +0200

    i9300: add selinux policies
    Change-Id: I0304c2efeb06b583a28ea9c9dcc874254ee3930f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,18 @@ TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
 TARGET_RECOVERY_FSTAB := device/samsung/i9300/rootdir/fstab.smdk4x12
 RECOVERY_FSTAB_VERSION := 2
 
+# Selinux
+BOARD_SEPOLICY_DIRS := \
+    device/samsung/i9300/selinux
+
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    rild.te \
+    wpa_supplicant.te
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := m0,i9300,GT-I9300
 
