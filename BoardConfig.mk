549a1c6690b905748645107c1a1099761849c8e9
forkbomb <keepcalm444@gmail.com>
2015-11-21 18:53:12 +1100

    i9300: squash RIL bringup
    * ril: update ril header to v11
      from https://github.com/ArchiDroid/android_device_samsung_i9300/commit/706bc3c354d2f6d6d905e6a26ccee282357c26ee
    * ril: LCE support
    * set BOARD_RIL_CLASS and add SamsungExynos4RIL
    * add Samsung symbols shim library
    * use ril-wrapper to stop libsec-ril from crashing
      (based off dmitry-ril for the Nexus S)
    
    Change-Id: I169024341621ea05db5b8d8e82c4bf958b7bc6c0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
+BOARD_RIL_CLASS := ../../../device/samsung/i9300/ril
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9300/rootdir/fstab.smdk4x12
