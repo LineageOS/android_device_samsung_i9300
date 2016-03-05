381a05756bdbe474ab76242a4e2934d81b8975e9
Simon Shields <keepcalm444@gmail.com>
2016-03-05 20:36:08 +1100

    i9300: rewrite libsensors in c++
    * based off t0lte sensors
    * AkmSensor kanged from i9100 (added sensor status support)
    * has a working compass!
    
    Change-Id: Iaed0463c33089ca0b636be4ba3f966f2e25f34f9

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -1,49 +1,47 @@
-# Copyright (C) 2013 Paul Kocialkowski <contact@paulk.fr>
+# Copyright (C) 2008 The Android Open Source Project
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-ifeq ($(BOARD_USES_OPENSOURCE_SENSORS),true)
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_SIMULATOR),true)
+
+# HAL module implemenation, not prelinked, and stored in
+# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	smdk4x12_sensors.c \
-	input.c \
-	akm8975.c \
-	akmdfs/AKFS_APIs_8975/AKFS_AK8975.c \
-	akmdfs/AKFS_APIs_8975/AKFS_AOC.c \
-	akmdfs/AKFS_APIs_8975/AKFS_Device.c \
-	akmdfs/AKFS_APIs_8975/AKFS_Direction.c \
-	akmdfs/AKFS_APIs_8975/AKFS_VNorm.c \
-	akmdfs/AKFS_FileIO.c \
-	cm36651_proximity.c \
-	cm36651_light.c \
-	lsm330dlc_acceleration.c \
-	lsm330dlc_gyroscope.c \
-	lps331ap.c
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/akmdfs \
-	$(LOCAL_PATH)/akmdfs/AKFS_APIs_8975 \
-	$(LOCAL_PATH)
-
-LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libhardware
-LOCAL_PRELINK_MODULE := false
-
 LOCAL_MODULE := sensors.$(TARGET_BOOTLOADER_BOARD_NAME)
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_CFLAGS := -DLOG_TAG=\"sensorscpp\"
+LOCAL_SRC_FILES :=  \
+        sensors.cpp \
+        SensorBase.cpp \
+        LightSensor.cpp	\
+        ProximitySensor.cpp	\
+        AkmSensor.cpp \
+        GyroSensor.cpp \
+        InputEventReader.cpp \
+        AccelSensor.cpp \
+        PressureSensor.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
-endif
+
+endif # !TARGET_SIMULATOR
