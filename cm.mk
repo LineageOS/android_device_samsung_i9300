9d3c6ff7cc0663644110db2fb238fdf704d13ecc
Simon Shields <keepcalm444@gmail.com>
2016-08-28 01:03:44 +1000

    i9300: clean up makefile inheritance
    Change-Id: I048628d325c9464fced30a3ba79dc1743d5cbfeb

--- cm.mk
+++ cm.mk
@@ -1,15 +1,19 @@
 # Release name
 PRODUCT_RELEASE_NAME := i9300
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/i9300/i9300.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Boot animation
 #TARGET_SCREEN_HEIGHT := 1280
 #TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
@@ -18,5 +22,9 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
 
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+		PRODUCT_NAME=m0xx \
+		TARGET_DEVICE=m0 \
+		BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" \
+		PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
